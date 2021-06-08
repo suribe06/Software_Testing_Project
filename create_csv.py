cd08bb16d9060f951583eb72dacc624db7c50c47
suribe06 <santiu990506@gmail.com>
2021-06-07 20:20:56 -0500

    actualizacion robots

--- create_csv.py
+++ create_csv.py
@@ -1,15 +1,26 @@
 import csv
 
 """
-Este funcion crea el archivo csv para las pruebas automatizadas y para las pruebas de estres y de carga
+Este archivo posee las funciones para crear los archivos csv para las pruebas automatizadas y para las pruebas de estres y de carga
 """
-fields = ["Nombres", "Apellidos", "Fecha de Nacimiento", "Tipo de Documento", "Numero Documento", "Departamento", "Municipio", "Barrio", "Direccion", "Genero", "Telefono", "Correo", "Usuario", "Contrasena", "Estrato"]
-filename = "data.csv"
 
-with open(filename, 'w') as csvfile:
-    csvwriter = csv.writer(csvfile)
-    csvwriter.writerow(fields)
-    for i in range(1000):
-        var = "prueba{0}".format(i)
-        row = [var, var, "07/06/2021", "CC", i, "Valle del Cauca", "Cali", "Caney", var, "Hombre", i, var+"@gmail.com", var, var, 3]
-        csvwriter.writerow(row)
+def create_data_register():
+    filename = "data_register.csv"
+    with open(filename, 'w') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        for i in range(1000):
+            var = "prueba{0}".format(i)
+            row = [var, var, i, var, i, var+"@gmail.com", var, var]
+            csvwriter.writerow(row)
+
+def create_data_login():
+    filename = "data_login.csv"
+    with open(filename, 'w') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        for i in range(1000):
+            var = "prueba{0}".format(i)
+            row = [var, var]
+            csvwriter.writerow(row)
+
+create_data_register()
+create_data_login()
--- robot_login.py
+++ robot_login.py
@@ -1,15 +1,29 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+import csv
 
 driver = Chrome()
 driver.get("http://127.0.0.1:5000/")
 
-#ingresa usuario
-driver.find_element_by_name('user').send_keys("prueba0")
-#ingresa Contrasena
-driver.find_element_by_name('pass').send_keys("prueba0")
+with open('data_login.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+        user = row[0]
+        passw = row[1]
+        #ingresa usuario
+        driver.find_element_by_name('user').send_keys(user)
+        #ingresa Contrasena
+        driver.find_element_by_name('pass').send_keys(passw)
 
-#dar click en el boton
-driver.find_element_by_id("login").click()
+        #dar click en el boton
+        driver.find_element_by_id("login").click()
+
+        #cerras sesion
+        element = driver.find_element_by_id('dropdownMenu2')
+        ActionChains(driver).click(element).perform()
+
+        #Cerrar sesion
+        driver.find_element_by_id("cs").click()
 
 driver.quit()
